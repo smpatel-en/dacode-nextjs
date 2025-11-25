ae5e95dd1cd163b30ac9ebcb326d8a3f47d0af9b
thesujalpatel <patelsujal24102004@gmail.com>
2025-11-25 12:55:44 +0530

    feat: adjust typography styles for h1 and h2 elements; enhance Button component shadow effects for improved visual hierarchy

--- src/components/core/Button.tsx
+++ src/components/core/Button.tsx
@@ -13,13 +13,13 @@ const defaultClass =
   "cursor-pointer rounded-full border-2 border-transparent font-bold tracking-[0.5%] transition-all duration-200 disabled:pointer-events-none disabled:opacity-20 active:scale-98";
 const variantClasses: Record<ButtonVariant, string> = {
   primary:
-    "bg-primary text-white hover:bg-dark shadow-primary active:shadow-sm",
+    "bg-primary text-white hover:bg-dark lg:shadow-primary active:shadow-sm",
   secondary: "bg-secondary text-white hover:bg-dark",
   "outline-primary":
     "border-primary text-primary hover:bg-primary hover:text-white",
   "outline-secondary":
     "border-secondary text-secondary hover:bg-secondary hover:text-white",
-  dark: "bg-dark text-white hover:bg-dark/80 hover:text-white shadow-primary-dark active:shadow-sm",
+  dark: "bg-dark text-white hover:bg-dark/80 hover:text-white lg:shadow-primary-dark active:shadow-sm",
   ghost: "backdrop-blur-lg text-dark hover:bg-dark/5",
 };
 const sizeClasses: Record<ButtonSize, string> = {
